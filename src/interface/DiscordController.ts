8deecd6d971e9ecc4d25b598ee235322b8b2a83e
EJ Mercado <ej@ejmercado.com>
2020-05-04 06:15:44 +0800

    fix dependencies for npm build

--- src/interface/DiscordController.ts
+++ src/interface/DiscordController.ts
@@ -1,8 +1,5 @@
 import { Notification } from './Notification'
 import { Channel } from './Channel'
-import { Guild, Client } from 'discord.js'
-import { DiscordController as DiscordControllerClass } from '../discord-controller'
-
 export interface DiscordController {
   send: (notification: Notification, channel: Channel) => Promise<void>
 }
