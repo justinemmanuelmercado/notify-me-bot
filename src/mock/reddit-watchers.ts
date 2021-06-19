419a1ffbe3835db31a18bfc712d8835e614a6e21
EJ Mercado <justinemmanuelmercado@gmail.com>
2021-06-19 10:02:40 +0000

    trying to run with a tester on

--- src/mock/reddit-watchers.ts
+++ src/mock/reddit-watchers.ts
@@ -1,34 +1,63 @@
-import { RedditWatcher, Criteria, CriteriaType } from '../interface/RedditWatcher'
+import {
+  RedditWatcher,
+  Criteria,
+  CriteriaType
+} from '../interface/RedditWatcher'
 import { GENERAL_CHANNEL } from './channels'
 
 const fgmCriteria: Criteria = [
-  new Map([
-    ['title', ['common projects', 'margiela', 'gat', 'achilles']]
-  ]),
-  new Map([
-    ['body', ['common projects', 'margiela', 'gat', 'achilles']]
-  ])
+  new Map([['title', ['common projects', 'margiela', 'gat', 'achilles']]]),
+  new Map([['body', ['common projects', 'margiela', 'gat', 'achilles']]])
 ]
 
 const forHireCriteria: Criteria = [
   new Map([
     ['title', ['Hiring', 'hiring']],
-    ['body', ['react', 'website', 'React', 'typescript', 'Typescript', 'node', 'nodejs', 'express', 'vue', 'frontend', 'backend']]
+    [
+      'body',
+      [
+        'react',
+        'website',
+        'React',
+        'typescript',
+        'Typescript',
+        'node',
+        'nodejs',
+        'express',
+        'vue',
+        'frontend',
+        'backend'
+      ]
+    ]
   ])
 ]
 
-// const tg4botsCriteria: Criteria = [
-//   new Map([
-//     ['title', ['[criteria]']],
-//     ['body', ['criteria 2', 'criteria 1']]
-//   ]),
-//   new Map([
-//     ['title', ['if this is in the title']],
-//     ['body', ['then this should be in the body', 'or this should be in the body']]
-//   ])
-// ]
+const tg4botsCriteria: Criteria = [
+  new Map([
+    ['title', ['[criteria]']],
+    ['body', ['criteria 2', 'criteria 1']]
+  ]),
+  new Map([
+    ['title', ['if this is in the title']],
+    [
+      'body',
+      ['then this should be in the body', 'or this should be in the body']
+    ]
+  ])
+]
 
 export const redditWatchers: RedditWatcher[] = [
+  {
+    type: 'post',
+    subreddit: 'testingground4bots',
+    action: 'match',
+    criteria: tg4botsCriteria,
+    channel: GENERAL_CHANNEL,
+    message: {
+      title: 'TESTING',
+      body: ''
+    }
+  },
   {
     type: 'post',
     subreddit: 'frugalmalefashion',
